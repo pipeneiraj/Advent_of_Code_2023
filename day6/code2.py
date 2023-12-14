d377d2f7b08be99e199f101e6d0d0345899f6795
Felipe Neira <fneiraj@gmail.com>
2023-12-13 23:48:59 -0500

    add code ejercice 2 day6

---
+++ day6/code2.py
@@ -0,0 +1,26 @@
+import time
+
+
+file = open("inputs\\input6.txt").read().replace(" ", "").split()
+
+time_ = int(file[0].split(":")[1])
+
+distance = int(file[1].split(":")[1])
+
+race = [time_, distance]
+
+
+def record(race: list[int]) -> int:
+    i = 0
+    while (race[0] - i) * i < race[1]:
+        i += 1
+    sum = race[0] - i * 2 + 1
+    return sum
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    print(record(race))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
